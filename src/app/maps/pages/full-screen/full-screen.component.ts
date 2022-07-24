5ae4271f4a427e0c527023d5de11407fb5297f06
Carlos Zabala <czabala847@gmail.com>
2022-07-24 16:51:54 -0500

    Terminando pagina fullscreen del mapa

--- src/app/maps/pages/full-screen/full-screen.component.ts
+++ src/app/maps/pages/full-screen/full-screen.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
+  styles: [
+    `
+      #map {
+        height: 100%;
+        width: 100%;
+      }
+    `,
+  ],
 })
 export class FullScreenComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.80050079279056, 10.92720608875137],
+      zoom: 16,
+    });
+  }
 }
