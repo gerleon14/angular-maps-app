6b7934d24b0825066e777a580e58c6f132664530
Carlos Zabala <czabala847@gmail.com>
2022-07-24 17:19:57 -0500

    Haciendo referencia al contenedor del map por medio de viewchield y controlando el zoom

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,11 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
+  styles: [
+    `
+      .map-container {
+        height: 100%;
+        width: 100%;
+      }
+
+      .row {
+        background-color: white;
+        bottom: 25px;
+        left: 25px;
+        padding: 20px;
+        border-radius: 5px;
+        position: fixed;
+        z-index: 999;
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements OnInit, AfterViewInit {
+  map!: mapboxgl.Map;
+
+  @ViewChild('map') mapDiv!: ElementRef;
+
   constructor() {}
 
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.80050079279056, 10.92720608875137],
+      zoom: 16,
+    });
+  }
+
   ngOnInit(): void {}
+
+  zoomIn() {
+    this.map.zoomIn();
+  }
+
+  zoomOut() {
+    this.map.zoomOut();
+  }
 }
