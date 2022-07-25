7b9ddd907f72cfb58b25bf8314660884892bd51e
Carlos Zabala <czabala847@gmail.com>
2022-07-25 12:29:07 -0500

    Creando componente minimapa para poder reutilizarlo

--- src/app/maps/components/min-map/min-map.component.ts
+++ src/app/maps/components/min-map/min-map.component.ts
@@ -1,11 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-min-map',
   templateUrl: './min-map.component.html',
+  styles: [
+    `
+      .map-container {
+        width: 100%;
+        height: 150px;
+      }
+    `,
+  ],
 })
-export class MinMapComponent implements OnInit {
+export class MinMapComponent implements AfterViewInit {
+  map!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  @Input() center: [number, number] = [0, 0];
+
+  @ViewChild('map') mapDiv!: ElementRef;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+      interactive: false,
+    });
+
+    new mapboxgl.Marker({}).setLngLat(this.center).addTo(this.map);
+  }
 }
